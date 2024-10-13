6ccefa81ea5aedc8cb911128f33aa6380e556403
YourUsername <rithvik.utd@outlook.com>
2024-10-13 16:08:09 -0400

    bug fixes-8

--- components/loader-wrapper/loader.tsx
+++ components/loader-wrapper/loader.tsx
@@ -4,62 +4,50 @@ import { easeOutExpo } from "@/constants/ease";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
-const greetings = [
-    "Hello!",
-    "Namaste!",
-    "Hola!",
-    "Bonjour!",
-    "Guten Tag!",
-    "Ciao!",
-];
+const greetings = ["Hello!", "Namaste!", "Hola!", "Bonjour!", , "Ciao!"];
 
 const Loader = () => {
-    const [greeting, setGreeting] = useState(
-        greetings[Math.floor(Math.random() * greetings.length)]
-    );
+  const [greeting, setGreeting] = useState(
+    greetings[Math.floor(Math.random() * greetings.length)]
+  );
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setGreeting(
-                greetings[Math.floor(Math.random() * greetings.length)]
-            );
-        }, 2500);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setGreeting(greetings[Math.floor(Math.random() * greetings.length)]);
+    }, 2500);
 
-        return () => clearInterval(interval);
-    }, []);
+    return () => clearInterval(interval);
+  }, []);
 
-    return (
-        <motion.div
-            exit={{ opacity: 0 }}
-            className="h-screen w-full flex justify-center items-center"
-        >
-            <AnimatePresence mode="popLayout">
-                <h1
-                    key={greeting}
-                    className="w-full text-center overflow-hidden"
-                >
-                    <motion.span
-                        variants={{
-                            initial: { y: "100%" },
-                            animate: { y: 0 },
-                            exit: { y: "-100%" },
-                        }}
-                        initial="initial"
-                        animate="animate"
-                        exit="exit"
-                        transition={{
-                            duration: 1,
-                            delay: 0.2,
-                            ease: easeOutExpo,
-                        }}
-                        className="block text-2xl md:text-4xl lg:text-6xl font-medium xl:text-7xl py-1 animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent"
-                    >
-                        {greeting}
-                    </motion.span>
-                </h1>
-            </AnimatePresence>
-        </motion.div>
-    );
+  return (
+    <motion.div
+      exit={{ opacity: 0 }}
+      className="h-screen w-full flex justify-center items-center"
+    >
+      <AnimatePresence mode="popLayout">
+        <h1 key={greeting} className="w-full text-center overflow-hidden">
+          <motion.span
+            variants={{
+              initial: { y: "100%" },
+              animate: { y: 0 },
+              exit: { y: "-100%" },
+            }}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 1,
+              delay: 0.2,
+              ease: easeOutExpo,
+            }}
+            className="block text-2xl md:text-4xl lg:text-6xl font-medium xl:text-7xl py-1 animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent"
+          >
+            {greeting}
+          </motion.span>
+        </h1>
+      </AnimatePresence>
+    </motion.div>
+  );
 };
 
 export default Loader;
